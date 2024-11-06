a4512c0699723565949dcf901162ffafbb5f8135
Renat Askarov <renat_askar@outlook.com>
2024-11-06 16:58:21 +0300

    Added: Task 2, Task 3

---
+++ Lesson 1/Task 2/Task2.cpp
@@ -0,0 +1,37 @@
+﻿#include "Windows.h"
+
+#include <iostream>
+#include <string>
+
+struct BankAccount
+{
+	int number{};
+	std::string name{};
+	double balance{};
+};
+
+void changeAccountBalance(BankAccount& bankAccount)
+{
+	std::cout << "Введите номер счёта: ";
+	std::cin >> bankAccount.number;
+
+	std::cout << "Введите имя владельца: ";
+	std::cin >> bankAccount.name;
+
+	std::cout << "Введите новый баланс: ";
+	std::cin >> bankAccount.balance;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	BankAccount owner{};
+
+	changeAccountBalance(owner);
+
+	std::cout << "Ваш счёт: " << owner.name << ", " << owner.number << ", " << owner.balance << '\n';
+
+	return 0;
+}
---
+++ Lesson 1/Task 3/Task3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+struct Address
+{
+	std::string cityName{};
+	std::string streetName{};
+	int houseNumber{};
+	int apartmentNumber{};
+	int index{};
+};
+
+void printAddress(Address& address)
+{
+	std::cout << "Город: " << address.cityName << '\n'
+	<< "Улица: " << address.streetName << '\n'
+	<< "Номер дома: " << address.houseNumber << '\n'
+	<< "Номер квартиры: " << address.apartmentNumber << '\n'
+	<< "Индекс: " << address.index << '\n';
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	Address address1{"Москва", "Арбат", 12, 8, 123456};
+	Address address2{"Ижевск", "Пушкина", 59, 143, 953769};
+
+	printAddress(address1);
+
+	std::cout << '\n';
+
+	printAddress(address2);
+
+	return 0;
+}
