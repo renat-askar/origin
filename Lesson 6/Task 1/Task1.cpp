de551c7278d62e5a09731485318cea8e64c0e87d
Renat Askarov <renat_askar@outlook.com>
2024-11-28 21:51:24 +0300

    Added Lesson 6

---
+++ Lesson 6/Task 1/Task1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+	#error необходимо определить MODE
+#endif
+
+#if MODE == 1
+	int add(int num1, int num2){return num1 + num2;}
+#endif
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	#if MODE == 0
+		std::cout << "Работаю в режиме тренировки\n";
+	#elif MODE == 1
+		std::cout << "Работаю в боевом режиме\n";
+
+		int num1{};
+		int num2{};
+
+		std::cout << "Введите число 1: ";
+		std::cin >> num1;
+
+		std::cout << "Введите число 2: ";
+		std::cin >> num2;
+
+		std::cout << "Результат сложения: " << add(num1, num2) << '\n';
+	#else
+		std::cout << "Неизвестный режим. Завершение работы\n";
+	#endif
+	
+	return 0;
+}
